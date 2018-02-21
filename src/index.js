32178927443433fdd3f17acc6749242270e02c91
Shamber Maxim <36640388+nowifi4u@users.noreply.github.com>
2018-02-21 19:40:03 +0300

    Update index.js
    'solution'

--- src/index.js
+++ src/index.js
@@ -1,30 +1,41 @@
 class Sorter {
   constructor() {
-    // your implementation
+      this.array = []
+      this.comparator = function(a, b) {
+          return a - b
+      }
   }
 
   add(element) {
-    // your implementation
+      this.array.push(element)
   }
 
   at(index) {
-    // your implementation
+      return this.array[index]
   }
 
   get length() {
-    // your implementation
+      return this.array.length
   }
 
   toArray() {
-    // your implementation
+      return this.array
   }
 
   sort(indices) {
-    // your implementation
+      let temp = []
+      for (let i = 0; i < indices.length; i++){
+          temp.push(this.array[indices[i]])
+      }
+      indices.sort()
+      temp.sort(this.comparator)
+      for (let i = 0; i < indices.length; i++){
+          this.array[indices[i]] = temp[i]
+      }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+      this.comparator = compareFunction;
   }
 }
 
